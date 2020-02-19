a713e671f73560f7edd782c65d140ddcbd9cb2ee
Ridwan Faturrahman <13517150@std.stei.itb.ac.id>
2020-02-19 19:30:37 +0700

    Button Goto Setting Activity

--- PhotoApp/app/src/main/java/com/ppl/photoapp/Fragment/CameraFragment.java
+++ PhotoApp/app/src/main/java/com/ppl/photoapp/Fragment/CameraFragment.java
@@ -1,18 +1,21 @@
 package com.ppl.photoapp.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.ppl.photoapp.R;
+import com.ppl.photoapp.SettingActivity;
 
 public class CameraFragment extends Fragment {
 
 
     public CameraFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -21,7 +24,21 @@ public class CameraFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
+        GotoSetttingActivity(view) ;
+
         return view ;
+    }
+
+    void GotoSetttingActivity(View view){
+        ImageView ivSetting = view.findViewById(R.id.ivSetting) ;
+        ivSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SettingActivity.class) ;
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
