e396698665c48d114c03704ccb5d777766d1009b
Ridwan Faturrahman <13517150@std.stei.itb.ac.id>
2020-02-19 19:00:03 +0700

    Add Camera Fragment

---
+++ PhotoApp/app/src/main/java/com/ppl/photoapp/Fragment/CameraFragment.java
@@ -0,0 +1,27 @@
+package com.ppl.photoapp.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ppl.photoapp.R;
+
+public class CameraFragment extends Fragment {
+
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        
+        return view ;
+    }
+
+}
