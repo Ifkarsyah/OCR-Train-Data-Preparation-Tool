ed9040548105d586bfcedebe788bbfdb9a4fafde
Ridwan Faturrahman <13517150@std.stei.itb.ac.id>
2020-02-19 19:01:46 +0700

    Add Gallery Fragment

--- PhotoApp/app/src/main/java/com/ppl/photoapp/Fragment/GalleryFragment.java
+++ PhotoApp/app/src/main/java/com/ppl/photoapp/Fragment/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.ppl.photoapp.Fragment;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,18 +9,17 @@ import android.view.ViewGroup;
 
 import com.ppl.photoapp.R;
 
-public class CameraFragment extends Fragment {
+public class GalleryFragment extends Fragment {
 
+    public GalleryFragment() {
 
-    public CameraFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
 
         return view ;
     }
