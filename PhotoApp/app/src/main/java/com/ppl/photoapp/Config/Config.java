b05f0b07b67d3de65f428a7baeed79bca44ee5e2
Ridwan Faturrahman <13517150@std.stei.itb.ac.id>
2020-02-20 17:14:25 +0700

    Update Config

--- PhotoApp/app/src/main/java/com/ppl/photoapp/Config/Config.java
+++ PhotoApp/app/src/main/java/com/ppl/photoapp/Config/Config.java
@@ -4,9 +4,7 @@ public class Config
 {
     public static final String ROOT_FOLDER = "Photo App" ;
     public static final String SUB_FOLDER = "Num-" ;
-
-    public static final String PREFIX_NAME_FILE = "File-" ;
-    public static final String SUFFIX_NAME_FILE = "Num-" ;
+    
     public static final String EXTENTION_FILE = ".jpg" ;
 
     public static final int[] FITLER_NUMBER = new int[]{-1,0,1,2,3,4,5,6,7,8,9} ;
