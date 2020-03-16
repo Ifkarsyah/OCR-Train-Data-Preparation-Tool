d07feaf7895c49f8e13d4bfead6473e7a6f65627
Ferdian Ifkarsyah <13517024@std.stei.itb.ac.id>
2020-03-16 22:00:10 +0700

    change unnamed constant

--- PhotoApp/app/src/main/java/com/ppl/photoapp/OpenCV/OpenCV.java
+++ PhotoApp/app/src/main/java/com/ppl/photoapp/OpenCV/OpenCV.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
 
 public class OpenCV
 {
+    private static final int COUNT_IMAGES_IN_ONE_ROW = 10;
+
     public static ArrayList<Bitmap> getArrayBitmap(Bitmap bitmapInput){
         // Input: bitmapInput == the whole image not splitted yet
 
         ArrayList<Bitmap> arrBitmap = new ArrayList<>() ;
         // TODO: change with working openCV implementation
-        for(int i = 0 ; i < 10*10 ; i ++ ){
+        for(int i = 0 ; i < COUNT_IMAGES_IN_ONE_ROW*10 ; i ++ ){
             // i = 0  .. 9  ==> get first row, will be labeled 0
             // i = 10 .. 19 ==> get second row, will be labeled 1
             // i = 20 .. 29 ==> get third row, will be labeled 2
@@ -30,10 +32,11 @@ public class OpenCV
         ArrayList<LabeledBitmapArray> arrLabeledBitmap = new ArrayList<>() ;
 
         //Dummy
-        for(int i = 0 ; i < arrBitmap.size()/10 ; i ++ ){
-            Bitmap[] bitmaps = new Bitmap[10] ;
+        for(int i = 0 ; i < arrBitmap.size()/COUNT_IMAGES_IN_ONE_ROW ; i ++ ){
+            Bitmap[] bitmaps = new Bitmap[COUNT_IMAGES_IN_ONE_ROW] ;
             for(int j = 0 ; j < bitmaps.length ; j ++ ){
-                bitmaps[j] = arrBitmap.get(i*10 + j) ;
+                int offsetRow = i*COUNT_IMAGES_IN_ONE_ROW;
+                bitmaps[j] = arrBitmap.get(offsetRow + j) ;
             }
             arrLabeledBitmap.add(new LabeledBitmapArray(bitmaps,i) );
         }
