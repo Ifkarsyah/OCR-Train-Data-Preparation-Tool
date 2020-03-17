5061e61caa5a4d5f29a6f4f578613154b3509eb8
Asif Hummam Rais <hashshura@gmail.com>
2020-03-17 08:58:35 +0700

    Implement opencv loader funct from skeleton [UNTESTED ON JAVA]

--- PhotoApp/app/src/main/java/com/ppl/photoapp/OpenCV/OpenCV.java
+++ PhotoApp/app/src/main/java/com/ppl/photoapp/OpenCV/OpenCV.java
@@ -5,27 +5,81 @@ import android.graphics.Bitmap;
 import com.ppl.photoapp.Model.LabeledBitmapArray;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 public class OpenCV
 {
-    private static final int COUNT_IMAGES_IN_ONE_ROW = 10;
+    private static final int COUNT_IMAGES_IN_ONE_ROW = 14;
 
     public static ArrayList<Bitmap> getArrayBitmap(Bitmap bitmapInput){
-        // Input: bitmapInput == the whole image not splitted yet
+        // Input: bitmapInput == the whole image not split yet
 
-        ArrayList<Bitmap> arrBitmap = new ArrayList<>() ;
-        // TODO: change with working openCV implementation
-        for(int i = 0 ; i < COUNT_IMAGES_IN_ONE_ROW*10 ; i ++ ){
-            // i = 0  .. 9  ==> get first row, will be labeled 0
-            // i = 10 .. 19 ==> get second row, will be labeled 1
-            // i = 20 .. 29 ==> get third row, will be labeled 2
-            //
-            // i = 90 .. 99 ==> get last row, will be labeled 9
-            arrBitmap.add(bitmapInput) ; // TODO: comment this
+        // Load image and resize into maximum of 1000px width
+        Mat orig = new Mat();
+        Bitmap bmp = bitmapInput.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bmp, orig);
+
+        int width = Math.min(1000, orig.cols());
+        int height = width / orig.rows() * orig.rows();
+        Size sz = new Size(width, height);
+
+        // Grayscale, adaptive threshold, and monochromatic invert from thresh
+        Mat image = new Mat();
+        Imgproc.resize(orig, image, sz , 0, 0, Imgproc.INTER_AREA);
+        Mat gray = new Mat();
+        Imgproc.cvtColor(image, gray, Imgproc.COLOR_BayerBG2GRAY);
+        Mat thresh = new Mat();
+        Imgproc.adaptiveThreshold(gray, thresh, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 57, 5);
+        Mat mono = new Mat();
+        Core.add(thresh, new Scalar(-255, -255, -255), mono);
+
+        // Filter out all numbers and noise to isolate only boxes
+        LinkedList<MatOfPoint> cnts = new LinkedList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(thresh, cnts, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        hierarchy.release();
+
+        LinkedList<MatOfPoint> tmp = new LinkedList<>();
+        for (MatOfPoint cnt : cnts){
+            Rect bb = Imgproc.boundingRect(cnt);
+            float ratio = ((float) bb.width)/bb.height;
+            if (ratio > 0.8 && ratio < 1.2)
+                tmp.add(cnt);
+        }
+        cnts = tmp;
+        Collections.sort(cnts, new Comparator<MatOfPoint>() {
+            @Override
+            public int compare(MatOfPoint o1, MatOfPoint o2) {
+                Rect rect1 = Imgproc.boundingRect(o1);
+                Rect rect2 = Imgproc.boundingRect(o2);
+                int result = Double.compare(rect1.tl().x, rect2.tl().x);
+                return result;
+            }
+        } );
+
+        ArrayList<Bitmap> arrBitmap = new ArrayList<>();
+        while (!cnts.isEmpty()){
+            MatOfPoint cnt = cnts.removeFirst();
+            Rect rect = Imgproc.boundingRect(cnt);
+            Mat outMap = new Mat(mono, rect);
+            Bitmap outBmp = Bitmap.createBitmap(outMap.cols(), outMap.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(outMap, outBmp);
+            arrBitmap.add(outBmp);
         }
 
         // Output: arrBitmap == ArrayList<Bitmap> == [0, 0, 0, 0, ...., 1, 1, 1, 1, ...., 9, 9, 9, 9, 9]
-        return arrBitmap ;
+        return arrBitmap;
     }
 
     public static ArrayList<LabeledBitmapArray> mappingBitmap(ArrayList<Bitmap> arrBitmap){
